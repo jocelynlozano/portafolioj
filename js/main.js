428fb974d32ed6f93c453b1e123814bd1eb823a9
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-12-07 18:15:58 -0500

    subiendo cambios

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function maquina(contenedor,texto,intervalo){
+   // Calculamos la longitud del texto
+   longitud = texto.length;
+   // Obtenemos referencia del div donde se va a alojar el texto.
+   cnt = document.getElementById(contenedor);
+   var i=0;
+   // Creamos el timer
+   timer = setInterval(function(){
+      // Vamos añadiendo letra por letra y la _ al final.
+      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
+      // Si hemos llegado al final del texto..
+      if(i >= longitud){
+         // Salimos del Timer y quitamos la barra baja (_)
+         clearInterval(timer);
+         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
+         return true;
+      } else {
+         // En caso contrario.. seguimos
+         i++;
+      }},intervalo);
+};
+
+var texto = "FRONT-END WEB DEVELOPER DESIGNER";
+maquina("maquinas",texto,100);
